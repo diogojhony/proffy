33fb592b5b70ab73cab4465a5af42d463c4bacf9
Diogo Jhony <diogojhony@gmail.com>
2020-08-06 15:01:00 -0300

    feature: Add classes registration

---
+++ server/src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+import ClassesController from './controllers/ClassesController';
+
+const classesController = new ClassesController();
+
+const routes = Router();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+export default routes;
