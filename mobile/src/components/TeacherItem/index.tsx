946c8fb085ae27a73c0fc8420897957bc9548411
Diogo Jhony <diogojhony@gmail.com>
2020-08-11 12:30:50 -0300

    feat: Create a new connection

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -8,6 +8,7 @@ import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -27,7 +28,11 @@ interface TeacherProps {
 const TeacherItem: React.FC<TeacherProps> = ({ teacher, favorited }) => {
   const [isFavorited, setIsFavorited] = useState(favorited);
 
-  function handleLinkToWhatsApp() {
+  async function handleLinkToWhatsApp() {
+    await api.post('connections', {
+      user_id: teacher.id,
+    });
+
     Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
   }
 
